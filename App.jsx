da4aecfd8fce854f115127636bd30630c96a0fd7
RohaibKhushi <khushirohaib@gmail.com>
2026-06-06 14:52:53 -0400

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -4,3 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+@font-face {
+  font-family: 'Acid Goth';
+  src: url('/fonts/AcidGoth.woff2') format('woff2'),
+       url('/fonts/AcidGoth.woff')  format('woff'),
+       url('/fonts/AcidGoth.ttf')   format('truetype');
+  src: url('/AcidGoth.woff2') format('woff2'),
+     url('/AcidGoth.ttf')   format('truetype');
